59a0952b0adbfef128804461051317560c426a39
getromandev <getroman.dev@gmail.com>
2022-06-29 20:30:24 -0400

    connect postgres DB and update config keys

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'hrcodes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': 5432,
+        'PASSWORD': config('POSTGRES_PASSWORD'),
     }
 }
 
