7ed988bdb5b5781aebc773bf043f2d716d11e3d8
getromandev <romanh99@gmail.com>
2022-10-18 21:51:44 -0400

    change debug setting to false

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['gentle-fortress-58982.herokuapp.com', 'hroman.codes', 'www.hroman.codes']
