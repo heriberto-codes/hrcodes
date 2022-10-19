517407f7650993fe8cdff2092dda82e64c25edae
getromandev <romanh99@gmail.com>
2022-10-19 13:49:32 -0400

    remove collectstatic from gitignore

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['gentle-fortress-58982.herokuapp.com', 'hroman.codes', 'www.hroman.codes']
