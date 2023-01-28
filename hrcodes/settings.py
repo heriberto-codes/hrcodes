7aeca3c5461f2f5c44903a16998db5af6cb179b1
getromandev <romanh99@gmail.com>
2023-01-28 15:41:50 -0500

    add AWS S3 credentialss

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -172,7 +172,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AWS Config to S3 Bucket 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'hroman-codes-portfolio-site'
 
 # Configure Django App for Heroku
 django_heroku.settings(locals())
