6139db17e50dbf979fb4798b48f6eff33a35655d
getromandev <romanh99@gmail.com>
2023-02-12 15:21:27 -0500

    add get method to retrieve dotenv

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -18,9 +18,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 load_dotenv()  # take environment variables from .env.
 
-# import environ
+import environ
 
-# env = environ.Env()
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
