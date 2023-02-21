1e4113076da410801bd43d2d0309d16fd0f70e84
getromandev <romanh99@gmail.com>
2023-02-21 02:05:06 -0500

    Remove the now ignored directory venv

--- hrcodes/views.py
+++
@@ -1,8 +0,0 @@
-import re
-from django.shortcuts import render
-
-def four_zero_four_page_not_found_view(request, exception):
-    return render(request, '404.html', status=404)
-
-def five_zero_five_page_not_found_view(request, exception):
-    return render(request, '500.html', status=500)
