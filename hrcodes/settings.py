b424b5eec86886fa2d75fd4b13dc7b0046a9e99a
getromandev <romanh99@gmail.com>
2023-02-25 02:13:27 -0500

    setup app for heroku

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://infinite-scrubland-19565.herokuapp.com', 'hroman.codes', 'www.hroman.codes', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['infinite-scrubland-19565.herokuapp.com', 'https://infinite-scrubland-19565.herokuapp.com', 'hroman.codes', 'www.hroman.codes', '127.0.0.1', 'localhost']
 
 # test 
 
@@ -178,6 +178,7 @@ MEDIA_URL = '/images/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if DEBUG is True:
+    ALLOWED_HOSTS = ['infinite-scrubland-19565.herokuapp.com', 'https://infinite-scrubland-19565.herokuapp.com', 'hroman.codes', 'www.hroman.codes', '127.0.0.1', 'localhost']
 #     ALLOWED_HOSTS = ['127.0.0.1']
 #     # Static files (CSS, JavaScript, Images)
 #     # https://docs.djangoproject.com/en/4.0/howto/static-files/ 
