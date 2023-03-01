1a87d6f8822252ae0a5abd8e3f50186befe86461
getromandev <romanh99@gmail.com>
2023-03-01 16:46:37 -0500

    update s3 config

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -151,6 +151,7 @@ AWS_S3_REGION_NAME = 'us-east-1'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_VERIFY = True
 
 # s3 static settings
 AWS_LOCATION = 'static'
