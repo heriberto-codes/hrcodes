f1cbc47bf2a66283ffaa2cf9ded4c8e0beb3dc58
getromandev <romanh99@gmail.com>
2023-03-01 16:51:01 -0500

    update s3 config

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 # AWS Settings for S3
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_DEFAULT_ACL = 'public-read'
