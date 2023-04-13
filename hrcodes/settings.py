f29d6c2f053081a2ab695d050dbcb43aaefeca02
getromandev <romanh99@gmail.com>
2023-04-13 00:07:05 -0400

    add max width to nav bar

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['infinite-scrubland-19565.herokuapp.com', 'https://infinite-scrubland-19565.herokuapp.com', 'hroman.codes', 'www.hroman.codes']
 
