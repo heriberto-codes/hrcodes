5d760cb3ba09d863373baeb16d03eded8abfd973
getromandev <romanh99@gmail.com>
2023-08-04 00:29:24 -0400

    update static

--- hrcodes/settings.py
+++ hrcodes/settings.py
@@ -174,26 +174,26 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# if DEBUG is True:
-#     ALLOWED_HOSTS = ['127.0.0.1']
+if DEBUG is True:
+    ALLOWED_HOSTS = ['127.0.0.1']
     
-#     STATIC_URL = '/static/'
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
     
-#     # SSL for dev development
-#     SECURE_SSL_REDIRECT = False
-#     SESSION_COOKIE_SECURE = False
-#     CSRF_COOKIE_SECURE = False
-#     SECURE_SSL_REDIRECT = False 
-#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # SSL for dev development
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+    SECURE_SSL_REDIRECT = False 
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
-#     DATABASES = {
-#         'default': {
-#             'ENGINE':'django.db.backends.sqlite3',
-#             'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
-#         }
-#     }
+    DATABASES = {
+        'default': {
+            'ENGINE':'django.db.backends.sqlite3',
+            'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+        }
+    }
     
     
